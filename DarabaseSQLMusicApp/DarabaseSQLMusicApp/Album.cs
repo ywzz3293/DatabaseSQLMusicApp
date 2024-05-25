501b9a8c7e0d04de390144c655ce2ac8c91a1473
ywzz3293 <50302188+ywzz3293@users.noreply.github.com>
2024-05-25 08:24:45 -0700

    Initial commit

---
+++ DarabaseSQLMusicApp/DarabaseSQLMusicApp/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarabaseSQLMusicApp
+{
+    internal class Album
+    {
+        public int ID { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public int Year { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+
+        //public list<Track> songs
+
+    }
+}
